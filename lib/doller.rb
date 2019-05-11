7c2e4ce9f68f3a1dd12998b743281b540a49ad84
Kuniko Sekine <32236068+kikois@users.noreply.github.com>
2019-05-12 02:11:52 +0900

    update : Override eql?

--- lib/doller.rb
+++ lib/doller.rb
@@ -8,4 +8,8 @@ class Doller
   def times(multiplier)
     Doller.new(@amount * multiplier)
   end
+
+  def eql?(other)
+    other.is_a?(Doller) && amount == other.amount
+  end
 end
--- spec/lib/doller_spec.rb
+++ spec/lib/doller_spec.rb
@@ -10,4 +10,18 @@ describe Doller do
       expect(product.amount).to eq 15
     end
   end
+
+  describe '#eql?' do
+    it do
+      object1 = Doller.new(5)
+      object2 = Doller.new(5)
+      expect(object1.eql?(object2)).to be true
+    end
+
+    it do
+      object1 = Doller.new(5)
+      object2 = Doller.new(6)
+      expect(object1.eql?(object2)).to be false
+    end
+  end
 end
