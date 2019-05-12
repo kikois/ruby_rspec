8abd5a245fef8ec8223df8aa9b3dde611abc95c8
Kuniko Sekine <32236068+kikois@users.noreply.github.com>
2019-05-12 22:48:29 +0900

    refactor: move doller spec to money spec

--- spec/lib/money_spec.rb
+++ spec/lib/money_spec.rb
@@ -1,2 +1,25 @@
 describe Money do
+  describe '#multiplication' do
+
+    it do
+      five = Doller.new(5)
+      expect(Doller.new(10)).to eq five.times(2)
+
+      expect(Doller.new(15)).to eq five.times(3)
+    end
+  end
+
+  describe '#eql?' do
+    it do
+      object1 = Doller.new(5)
+      object2 = Doller.new(5)
+      expect(object1.eql?(object2)).to be true
+    end
+
+    it do
+      object1 = Doller.new(5)
+      object2 = Doller.new(6)
+      expect(object1.eql?(object2)).to be false
+    end
+  end
 end
